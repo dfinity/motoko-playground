5da2292de92699f9d4027a712b33f60677013452
Yan Chen <48968912+chenyan-dfinity@users.noreply.github.com>
2020-09-23 16:09:58 -0700

    Candid UI (#3)
    * web UI with modified bootstrap
    
    * management canister
    
    * update moc and iframe
    
    * add did_to_js generated from Rust CDK
    
    * add candid ui
    
    * add id

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import assets from 'ic:canisters/playground_assets';
+import didjs from 'ic:canisters/didjs';
 import * as Wasi from './wasiPolyfill';
-import { Actor, blobFromUint8Array } from '@dfinity/agent';
+import { Actor, blobFromUint8Array, Principal } from '@dfinity/agent';
+import ic_idl from './management';
+import { fetchActor, render } from './candid';
+import './candid.css';
 
 const prog = `import Time "mo:base/Time";
 import Prim "mo:prim";
 actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # " at " # (debug_show Time.now());
+  var c = Time.now();
+  public query func greet(name : Text) : async Text {
+    let uptime = (Time.now() - c)/1000000;
+    return "Hello, " # name # " at " # (debug_show uptime) # "ms";
   };
+  public func add() : async Int { c += 1; c };
 };
 `;
 
@@ -22,11 +29,14 @@ async function loadBase(lib) {
 
 let output;
 let editor;
+let overlay;
+let hide;
+const ic0 = Actor.createActor(ic_idl, { canisterId: Principal.fromHex('') });
 
 function initUI() {
   const dom = document.createElement('div');
   dom.width = "100%";
-  dom.style = "width:100%;display:flex;align-items:stretch";
+  dom.style = "width:100%;display:flex;align-items:stretch; position:relative";
   document.body.appendChild(dom);
   
   const code = document.createElement('div');
@@ -34,8 +44,12 @@ function initUI() {
   code.style = "height:400px;width:50%;border:1px solid black;";
 
   output = document.createElement('textarea');
-  output.style.width = "45%";
+  output.style.width = "50%";
   output.value = "Loading...(Do nothing before you see 'Ready')\n";
+
+  overlay = document.createElement('div');
+  overlay.style = "position:absolute; top:4em; right:0; z-index:10; width:50%; height: 80%; visibility:hidden; overflow:scroll;";
+  
   const run = document.createElement('input');
   run.type = "button";
   run.value = "Run";
@@ -45,13 +59,29 @@ function initUI() {
   const ic = document.createElement('input');
   ic.type = "button";
   ic.value = "Deploy on IC";
+  // Hide button
+  hide = document.createElement('input');
+  hide.type = "button";
+  hide.value = "Hide UI";
   
   dom.appendChild(code);
   dom.appendChild(output);
+  dom.appendChild(overlay);
   document.body.appendChild(run);
   document.body.appendChild(compile);
   document.body.appendChild(ic);
+  document.body.appendChild(hide);  
 
+  hide.addEventListener('click', () => {
+    if (overlay.style.visibility === 'visible') {
+      overlay.style.visibility = 'hidden';
+      hide.value = 'Show UI';
+    } else {
+      overlay.style.visibility = 'visible';
+      hide.value = 'Hide UI';
+    }
+  });
+  
   run.addEventListener('click', () => {
     output.value = 'Running...';
     try {
@@ -104,7 +134,32 @@ function initUI() {
           const canisterId = await Actor.createCanister();
           output.value += `Created canisterId ${canisterId}\n`;
           await Actor.install({ module: blobFromUint8Array(wasm) }, { canisterId });
-          output.value += `Ready.\nUse "dfx canister --network tungsten call ${canisterId} method arguments" to communicate with the canister before Candid UI lands in Tungsten.\n`;
+          output.value += `Code installed\n`;
+          const actor = await fetchActor(canisterId);
+          overlay.style.visibility = 'visible';
+          render(overlay, canisterId, actor);
+          /*
+          const url = window.location.origin + `/candid?canisterId=${canisterId}`;
+          overlay.src = url;
+          overlay.style.visibility = 'visible';*/
+          // close button
+          const close = document.createElement('input');
+          close.type = 'button';
+          close.value = 'Delete canister';
+          document.body.appendChild(close);
+          close.addEventListener('click', () => {
+            (async () => {
+              if (overlay.style.visibility === 'visible') {
+                hide.click();
+              }
+              output.value += 'Deleting canister...\n';
+              await ic0.stop_canister({ canister_id: canisterId });
+              output.value += 'Canister stopped\n';
+              await ic0.delete_canister({ canister_id: canisterId });
+              output.value += 'Canister deleted\n';
+              close.remove();
+            })();
+          });
         })().catch(err => {
           output.value += 'IC Exception:\n' + err.stack;
           throw err;
