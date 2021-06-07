7fb803a09702d9b649e1c37a8ca2255dfdd9807e
chenyan-dfinity <yan.chen@dfinity.org>
2021-06-06 20:31:31 -0700

    fix

--- src/agent.js
+++ src/agent.js
@@ -1,5 +1,17 @@
-import { HttpAgent } from '@dfinity/agent';
+import { HttpAgent, makeNonceTransform } from '@dfinity/agent';
 import { Ed25519KeyIdentity } from '@dfinity/identity';
 
+function is_local(hostname) {
+  return hostname === '127.0.0.1' || hostname.endsWith('localhost');
+}
+
 const identity = Ed25519KeyIdentity.generate();
-export const agent = new HttpAgent({identity});
+export const agent = Promise.resolve(new HttpAgent({identity})).then(
+  async agent => {
+    if (is_local(agent._host.hostname)) {
+      await agent.fetchRootKey();
+    }
+    agent.addTransform(makeNonceTransform());
+    return agent;
+  }
+);
