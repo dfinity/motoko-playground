586de4e13adf908b1b791687a4ffbbabda89cb57
chenyan-dfinity <yan.chen@dfinity.org>
2021-06-07 19:24:43 -0700

    fix

--- src/agent.js
+++ src/agent.js
@@ -24,7 +24,7 @@ initAgent();
 export const ic0 = Actor.createActor(ic_idl, { agent, canisterId: Principal.fromHex('') });
 // TODO: With DNS, we don't need to hard code the canister id.
 const ui_canister_id = is_local(agent)?'ryjl3-tyaaa-aaaaa-aaaba-cai':'a4gq6-oaaaa-aaaab-qaa4q-cai';
-export const ui_canister_url = is_local(agent)?`?canisterId=${ui_canister_id}&`:`https://${ui_canister_id}.ic0.app/`;
+export const ui_canister_url = is_local(agent)?`?canisterId=${ui_canister_id}&`:`https://${ui_canister_id}.raw.ic0.app/?`;
 const didjs = Actor.createActor(didjs_idl, { agent, canisterId: Principal.fromText(ui_canister_id) });
 export async function didToJs(source) {
   const js = await didjs.did_to_js(source);
@@ -53,7 +53,7 @@ class Wallet {
         freezing_threshold: [],
         memory_allocation: [],
       },
-      cycles: BigInt(1_000_000),      
+      cycles: BigInt(105_000_000_000),      
     });
     if ("Ok" in result) {
       return result.Ok.canister_id;
@@ -63,7 +63,6 @@ class Wallet {
   }
   async forwardCall(canisterId, method, func, ...args) {
     const encoded = bufferToArray(IDL.encode(func.argTypes, args));
-    console.log(encoded);
     const result = await this._wallet.wallet_call({
       args: encoded,
       cycles: BigInt(1),
@@ -82,4 +81,6 @@ class Wallet {
   }
 }
 
-export const wallet = new Wallet(Principal.fromText('rwlgt-iiaaa-aaaaa-aaaaa-cai'));
+// TODO add a config UI to update wallet id
+const wallet_id = is_local(agent)?'rwlgt-iiaaa-aaaaa-aaaaa-cai':'a3hwk-dyaaa-aaaab-qaa4a-cai';
+export const wallet = new Wallet(Principal.fromText(wallet_id));
