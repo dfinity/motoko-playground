beec1a9180da0fce0ec8935f4b885d9a2f6e975b
= <taylor.ham@dfinity.org>
2021-06-14 23:10:47 -0700

    dark mode test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,11 @@ import styled from "styled-components";
 import motokoLabLogo from "../assets/images/motoko-lab-logo.png";
 import motokoLabWordmark from "../assets/images/motoko-lab-wordmark.png";
 
-const StyledHeader = styled.header`
+const StyledHeader = styled("header")<{ darkMode?: boolean }>`
   width: 100%;
   height: var(--headerHeight);
   padding: 1.6rem;
-  background-color: white;
+  background-color: ${(props) => (props.darkMode ? "black" : "white")};
 `;
 
 const BrandContainer = styled.div`
@@ -16,23 +16,36 @@ const BrandContainer = styled.div`
   height: 6.4rem;
 `;
 
-const Logo = styled.img`
+const Logo = styled("img")<{ darkMode?: boolean }>`
   margin: -1.6rem;
   margin-left: -1.2rem;
   width: 10.9rem;
+  ${(props) => (props.darkMode ? "filter: brightness(1.7) saturate(0.9);" : "")}
 `;
 
-const WordMark = styled.img`
+const WordMark = styled("img")<{ darkMode?: boolean }>`
   margin-left: 2.4rem;
   height: 2.4rem;
+  ${(props) =>
+    props.darkMode
+      ? "filter: brightness(3) saturate(0.75) hue-rotate(-14deg);"
+      : ""}
 `;
 
-export function Header() {
+export function Header({ darkMode = false }) {
   return (
-    <StyledHeader>
+    <StyledHeader darkMode={darkMode}>
       <BrandContainer>
-        <Logo src={motokoLabLogo} alt="Motoko Ghost Logo in wireframe" />
-        <WordMark src={motokoLabWordmark} alt="Motoko Lab" />
+        <Logo
+          darkMode={darkMode}
+          src={motokoLabLogo}
+          alt="Motoko Ghost Logo in wireframe"
+        />
+        <WordMark
+          darkMode={darkMode}
+          src={motokoLabWordmark}
+          alt="Motoko Lab"
+        />
       </BrandContainer>
     </StyledHeader>
   );
