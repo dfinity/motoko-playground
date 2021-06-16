43e664b99fd261f2ddebf8c215032cf9c66808e3
= <taylor.ham@dfinity.org>
2021-06-16 16:24:50 -0700

    add original codebase for posterity/reference

---
+++ original/agent.js
@@ -0,0 +1,87 @@
+import { Actor, HttpAgent } from '@dfinity/agent';
+import { Principal } from '@dfinity/principal';
+import { IDL } from '@dfinity/candid';
+import { Ed25519KeyIdentity } from '@dfinity/identity';
+import wallet_idl from './wallet.did';
+import ic_idl from './ic.did';
+import didjs_idl from './didjs.did';
+
+function is_local(agent) {
+  const hostname = agent._host.hostname;
+  return hostname === '127.0.0.1' || hostname.endsWith('localhost');
+}
+
+const seed = new Array(32).fill(0);
+const identity = Ed25519KeyIdentity.generate(new Uint8Array(seed));
+console.log(identity.getPrincipal().toText());
+export const agent = new HttpAgent({identity});
+async function initAgent() {
+  if (is_local(agent)) {
+    await agent.fetchRootKey();
+    console.log('fetchRootKey');
+  }  
+}
+initAgent();
+
+export const ic0 = Actor.createActor(ic_idl, { agent, canisterId: Principal.fromHex('') });
+// TODO: With DNS, we don't need to hard code the canister id.
+const ui_canister_id = is_local(agent)?'ryjl3-tyaaa-aaaaa-aaaba-cai':'a4gq6-oaaaa-aaaab-qaa4q-cai';
+export const ui_canister_url = is_local(agent)?`?canisterId=${ui_canister_id}&`:`https://${ui_canister_id}.raw.ic0.app/?`;
+const didjs = Actor.createActor(didjs_idl, { agent, canisterId: Principal.fromText(ui_canister_id) });
+export async function didToJs(source) {
+  const js = await didjs.did_to_js(source);
+  if (js === []) {
+    return undefined;
+  }
+  const dataUri = 'data:text/javascript;charset=utf-8,' + encodeURIComponent(js[0]);
+  const candid = await eval('import("' + dataUri + '")');
+  return candid;
+}
+
+class Wallet {
+  constructor(canisterId) {
+    this._canisterId = canisterId;
+    this._wallet = Actor.createActor(wallet_idl, { agent, canisterId });
+  }
+  async createCanister(config) {
+    const result = await this._wallet.wallet_create_canister({
+      settings: {
+        compute_allocation: [],
+        controller: [],
+        freezing_threshold: [],
+        memory_allocation: [],
+      },
+      cycles: BigInt(105_000_000_000),      
+    });
+    if ("Ok" in result) {
+      return result.Ok.canister_id;
+    } else {
+      throw result.Err;
+    }
+  }
+  async forwardCall(canisterId, method, func, ...args) {
+    const encoded = IDL.encode(func.argTypes, args);
+    const result = await this._wallet.wallet_call({
+      args: [...encoded],
+      cycles: BigInt(1),
+      method_name: method,
+      canister: canisterId,
+    });
+    if ("Ok" in result) {
+      return IDL.decode(func.retTypes, Buffer.from(result.Ok.return));
+    } else {
+      throw result.Err;
+    }
+  }
+  async forwardManagement(method, canisterId, ...args) {
+    const func = Object.fromEntries(Actor.interfaceOf(ic0)._fields)[method];
+    if (!func) {
+      throw new Error(`${method} not found in management canister.`);
+    }
+    return await this.forwardCall(Principal.fromHex(''), method, func, ...args);
+  }
+}
+
+// TODO add a config UI to update wallet id
+const wallet_id = is_local(agent)?'rwlgt-iiaaa-aaaaa-aaaaa-cai':'a3hwk-dyaaa-aaaab-qaa4a-cai';
+export const wallet = new Wallet(Principal.fromText(wallet_id));
---
+++ original/file.js
@@ -0,0 +1,98 @@
+import { setMarkers, editor } from './monaco';
+import { log, clearLogs } from './log';
+import { canister, canister_ui } from './build';
+
+// map filepath to code session { state, model }
+export const files = {};
+export var current_session_name = "main.mo";
+export const filetab = document.createElement('div');
+
+export function addFile(name, content) {
+  const model = monaco.editor.createModel(content, 'motoko');
+  files[name] = {model, state: null};
+  addFileEntry('file', name, model);
+  let handle;
+  model.onDidChangeContent(() => {
+    clearTimeout(handle);
+    handle = setTimeout(() => {
+      /*const proxy = worker.getProxy();
+      proxy.then((p) => {
+        p.doValidate(null).then((r) => { console.log(r) })
+      });*/
+      saveCodeToMotoko();
+      const diags = Motoko.check(name).diagnostics;
+      setMarkers(diags);
+    }, 500);
+  });
+}
+
+export function addFileEntry(type, name, model) {
+  const entry = document.createElement('button');
+  entry.innerText = name;
+  if (type === 'package') {
+    entry.style = 'color:blue';
+  } else if (type === 'canister') {
+    entry.style = 'color:green';
+  }
+  filetab.appendChild(entry);
+  entry.addEventListener('click', () => {
+    for (const e of filetab.children) {
+      e.className = '';
+    }
+    entry.className = 'active';
+    if (type === 'canister') {
+      clearLogs();      
+      const ui = canister_ui[name.slice(9)];
+      log(ui);
+    } else {
+      if (files[current_session_name]) {
+        files[current_session_name].state = editor.saveViewState();
+      }
+      if (files[name] && files[name].state) {
+        editor.restoreViewState(files[name].state);
+      }
+      editor.setModel(model);
+      current_session_name = name;
+    }
+  });
+  return entry;
+}
+
+export async function addPackage(name, repo, version, dir) {
+  const meta_url = `https://data.jsdelivr.com/v1/package/gh/${repo}@${version}/flat`;
+  const base_url = `https://cdn.jsdelivr.net/gh/${repo}@${version}`;
+  const response = await fetch(meta_url);
+  const json = await response.json()
+  const promises = [];
+  const fetchedFiles = [];
+  for (const f of json.files) {
+    if (f.name.startsWith(`/${dir}/`) && /\.mo$/.test(f.name)) {
+      const promise = (async () => {
+        const content = await (await fetch(base_url + f.name)).text();
+        const stripped = name + f.name.slice(dir.length + 1);
+        fetchedFiles.push(stripped);
+        Motoko.saveFile(stripped, content);
+      })();
+      promises.push(promise);
+    }
+  }
+  Promise.all(promises).then(() => {
+    Motoko.addPackage(name, name + '/');
+    log(`Package ${name} loaded (${promises.length} files).`)
+    // add ui
+    const content = [`// Fetched from ${repo}@${version}/${dir}`, ...fetchedFiles.map(s => `mo:${s.slice(0,-3)}`)].join('\n');
+    const model = monaco.editor.createModel(content, 'motoko');
+    addFileEntry('package', `mo:${name}`, model);
+  });
+}
+
+export function saveCodeToMotoko() {
+  for (const [name, session] of Object.entries(files)) {
+    Motoko.saveFile(name, session.model.getValue());
+  }
+  const aliases = [];
+  for (const [name, id] of Object.entries(canister)) {
+    aliases.push([name, id.toText()]);
+  }
+  Motoko.setActorAliases(aliases);
+}
