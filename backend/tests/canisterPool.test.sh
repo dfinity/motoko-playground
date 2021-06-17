7383e736ea856f5c9e2ac134138b627d3294cc0e
chenyan-dfinity <yan.chen@dfinity.org>
2021-06-16 22:53:47 -0700

    fix

--- backend/tests/canisterPool.test.sh
+++ backend/tests/canisterPool.test.sh
@@ -2,7 +2,7 @@
 
 import S = "rwlgt-iiaaa-aaaaa-aaaaa-cai";
 
-Let c1 = call S.getCanisterId();
+let c1 = call S.getCanisterId();
 let c2 = call S.getCanisterId();
 let c3 = call S.getCanisterId();
 let c4 = call S.getCanisterId();
