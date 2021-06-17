922dfda50f85c8d440b4d8e3845b549bdc9aeb25
chenyan-dfinity <yan.chen@dfinity.org>
2021-06-16 22:40:15 -0700

    backend CI

---
+++ backend/tests/canisterPool.test.sh
@@ -0,0 +1,9 @@
+#!ic-repl -r http://localhost:8000
+
+import S = "rwlgt-iiaaa-aaaaa-aaaaa-cai";
+
+Let c1 = call S.getCanisterId();
+let c2 = call S.getCanisterId();
+let c3 = call S.getCanisterId();
+let c4 = call S.getCanisterId();
+call S.dump();
