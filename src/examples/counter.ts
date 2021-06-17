2813768ac022f0344c3f0c69728683d9658c63d2
Benjamin Goering <benjamin.goering@dfinity.org>
2021-06-17 12:53:37 -0700

    Update counter README to include more links

--- src/examples/counter.ts
+++ src/examples/counter.ts
@@ -3,20 +3,27 @@ import { ExampleProject, FileContent } from "./types";
 const counterReadme: FileContent = `
 # Counter
 
-This example demonstrates a counter application. It uses an [orthogonally
-persistent](https://sdk.dfinity.org/docs/language-guide/motoko.html#_orthogonal_persistence)
-[stable](https://sdk.dfinity.org/docs/language-guide/upgrades.html) \`counter\` variable to store an arbitrary precision natural number
-that represents the current value of the counter.
+This example demonstrates a counter application. It uses an orthogonally persistent stable \`counter\` variable to store a natural number that represents the current value of the counter.
 
-## Introduction
+By using the Motoko keyword \`stable\` when declaring the \`counter\` variable,
+the value of this variable will automatically be preserved whenever your canister code is
+upgraded. Without the \`stable\` keyword, a variable is deemed \`flexible\`, and its value
+is reinitialized on every canister upgrade, i.e. whenever new code is deployed to the canister.
+
+To learn more about these features of Motoko, see:
+* https://sdk.dfinity.org/docs/language-guide/motoko.html#_orthogonal_persistence
+* https://sdk.dfinity.org/docs/language-guide/upgrades.html#_declaring_stable_variables
+
+## Overview
 
 The application provides an interface that exposes the following methods:
 
-*  \`set\`, which sets the value of the counter;
+*  \`get\`, which gets the value of the counter by returning the value of the actor's private \`counter\` variable.
 
-*  \`inc\`, which increments the value of the counter; and
+*  \`set\`, which sets the value of the counter by assigning a new value to the private \`counter\` variable.
 
-*  \`get\`, which gets the value of the counter.
+*  \`inc\`, which increments the value of the counter by one
+
 `.trim()
 
 const counterMain: FileContent = `
