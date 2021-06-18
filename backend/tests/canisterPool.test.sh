ddc207413e417ba726739c914a36f3f852161cc6
chenyan-dfinity <yan.chen@dfinity.org>
2021-06-17 20:21:04 -0700

    fix

--- backend/tests/canisterPool.test.sh
+++ backend/tests/canisterPool.test.sh
@@ -1,13 +1,22 @@
 #!ic-repl
 
+let id = call ic.provisional_create_canister_with_cycles(record { settings = null; amount = null });
+let S = id.canister_id;
+
 // Immediately expire
-let cycles = (100_000_000_000_000 : nat);
 let init = opt record {
   cycles_per_canister = 105_000_000_000;
   max_num_canisters = 2;
   TTL = 1;
 };
-load "install.sh";
+call ic.install_code(
+  record {
+    arg = encode (init);
+    wasm_module = file "../../.dfx/local/canisters/backend/backend.wasm";
+    mode = variant { install };
+    canister_id = S;
+  },
+);
 let c1 = call S.getCanisterId();
 c1;
 let c2 = call S.getCanisterId();
@@ -25,15 +34,37 @@ let init = opt record {
   max_num_canisters = 2;
   TTL = 60_000_000_000;
 };
-load "install.sh";
+call ic.install_code(
+  record {
+    arg = encode (init);
+    wasm_module = file "../../.dfx/local/canisters/backend/backend.wasm";
+    mode = variant { reinstall };
+    canister_id = S;
+  },
+);
 call S.getCanisterId();
 call S.getCanisterId();
 fail call S.getCanisterId();
 assert _ ~= "No available canister id";
 
 // Out of cycle
-let cycles = (10_000_000 : nat);
+let id = call ic.provisional_create_canister_with_cycles(record { settings = null; amount = opt 10_000_000 });
+let S = id.canister_id;
 let init = (null : opt record {});
-load "install.sh";
+call ic.install_code(
+  record {
+    arg = encode (init);
+    wasm_module = file "../../.dfx/local/canisters/backend/backend.wasm";
+    mode = variant { install };
+    canister_id = S;
+  },
+);
 fail call S.getCanisterId();
 assert _ ~= "from a call when only 10000000 was available";
+call ic.provisional_top_up_canister(
+  record {
+    canister_id = S;
+    amount = 100_000_000_000_000;
+  },
+);
+call S.getCanisterId();
