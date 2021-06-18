7e7a2af09cfc70344b1ec6c7973dfe911724d4a0
chenyan-dfinity <yan.chen@dfinity.org>
2021-06-17 19:00:48 -0700

    init args for CI

--- backend/tests/canisterPool.test.sh
+++ backend/tests/canisterPool.test.sh
@@ -1,15 +1,31 @@
 #!ic-repl
 
+// Immediately expire
 let init = record {
   cycles_per_canister = 105_000_000_000;
   max_num_canisters = 2;
-  TTL = 10;
+  TTL = 1;
 };
 load "install.sh";
 let c1 = call S.getCanisterId();
+c1;
 let c2 = call S.getCanisterId();
+c2;
 let c3 = call S.getCanisterId();
+c3;
 let c4 = call S.getCanisterId();
+c4;
 assert c1.id == c3.id;
 assert c2.id == c4.id;
 
+// Out of capacity
+let init = record {
+  cycles_per_canister = 105_000_000_000;
+  max_num_canisters = 2;
+  TTL = 60_000_000_000;
+};
+load "install.sh";
+call S.getCanisterId();
+call S.getCanisterId();
+fail call S.getCanisterId();
+assert _ ~= "No available canister id";
