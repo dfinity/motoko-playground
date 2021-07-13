728e69f79d4c3538296c5a7942381495fd22adc6
chenyan-dfinity <yan.chen@dfinity.org>
2021-07-12 17:34:20 -0700

    fix ci

--- wasm-utils/build.sh
+++ wasm-utils/build.sh
@@ -4,6 +4,8 @@ set -e
 
 if [ "$CI" == "true" ];
 then
+  mkdir -p target/wasm32-unknown-unknown/release
+  touch target/wasm32-unknown-unknown/release/wasm_opt.wasm
   echo "skip"
 else
   cargo build --target wasm32-unknown-unknown --release --package wasm-utils
