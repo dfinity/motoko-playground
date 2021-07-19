570503e25df9bf7622818cfb708969a827778cdd
chenyan-dfinity <yan.chen@dfinity.org>
2021-07-19 15:27:17 -0700

    remove legacy code

--- original/didjs.did.js
+++
@@ -1,6 +0,0 @@
-export default ({ IDL }) => {
-  return IDL.Service({
-    'did_to_js' : IDL.Func([IDL.Text], [IDL.Opt(IDL.Text)], ['query']),
-  });
-};
-export const init = ({ IDL }) => { return []; };
