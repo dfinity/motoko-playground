515ae4d827fccaa5540a66232624e0e227efeef5
Kento Sugama <kento.sugama@dfinity.org>
2022-08-12 15:40:09 -0700

    ci debug

--- service/wasm-utils/build.sh
+++ service/wasm-utils/build.sh
@@ -2,13 +2,5 @@
 
 set -e
 
-if [ "$CI" == "true" ];
-then
-  mkdir -p target/wasm32-unknown-unknown/release
-  echo -n -e '\x00\x61\x73\x6d\x01\x00\x00\x00' > target/wasm32-unknown-unknown/release/wasm_opt.wasm
-  echo "skip"
-else
-  cargo build --target wasm32-unknown-unknown --release --package wasm-utils
-  wasm-opt --strip-debug target/wasm32-unknown-unknown/release/wasm_utils.wasm -o target/wasm32-unknown-unknown/release/wasm_opt.wasm
-fi
-
+cargo build --target wasm32-unknown-unknown --release --package wasm-utils
+wasm-opt --strip-debug target/wasm32-unknown-unknown/release/wasm_utils.wasm -o target/wasm32-unknown-unknown/release/wasm_opt.wasm
