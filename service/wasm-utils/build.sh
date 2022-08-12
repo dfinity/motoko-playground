532c56f279ca893994a8cff9061b1d9c702b8d8d
Kento Sugama <kento.sugama@dfinity.org>
2022-08-12 15:46:11 -0700

    ci debug

--- service/wasm-utils/build.sh
+++ service/wasm-utils/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-set -e
-
 cargo build --target wasm32-unknown-unknown --release --package wasm-utils
 wasm-opt --strip-debug target/wasm32-unknown-unknown/release/wasm_utils.wasm -o target/wasm32-unknown-unknown/release/wasm_opt.wasm
