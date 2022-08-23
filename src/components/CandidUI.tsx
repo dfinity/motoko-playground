bace8318c821bb7241f5898698cc5e21cccf31c5
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-08-22 22:29:36 -0600

    Implement `postMessage()` configuration for embedded Candid UI (#125)
    * Fix type error in external editor integration
    
    * Enable receiving messages from Candid UI
    
    * Pass Candid data via postMessage() if too long for query param
    
    * Remove encodeURIComponent during postMessage configuration
    
    * Implement external-config for pop-out UI
    
    * update new management canister interface
    
    Co-authored-by: Yan Chen <48968912+chenyan-dfinity@users.noreply.github.com>
    Co-authored-by: Yan Chen <yan.chen@dfinity.org>

--- src/components/CandidUI.tsx
+++ src/components/CandidUI.tsx
@@ -3,7 +3,7 @@ import { PanelHeader } from "./shared/PanelHeader";
 import iconCollapse from "../assets/images/icon-collapse.svg";
 import iconOpen from "../assets/images/icon-open.svg";
 import { uiCanisterUrl } from "../config/actor";
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, useCallback } from "react";
 
 const CandidPanel = styled.div<{ isExpanded: boolean }>`
   width: 40%;
@@ -55,22 +55,16 @@ export function CandidUI({
   const [isExpanded, setIsExpanded] = useState(true);
   const candidFrameRef = useRef<HTMLIFrameElement>(null);
   const didParam =
-    candid && candid.length < 2048
+    candid && candid.length < 2000
       ? `&did=${encodeURIComponent(btoa(candid))}`
-      : "";
+      : "&external-config";
 
   const url =
     `${CANDID_UI_CANISTER_URL}/?id=${canisterId}&tag=${forceUpdate}` + didParam;
 
-  useEffect(() => {
-    const newSize = isExpanded ? "30vw" : "fit-content";
-    setCandidWidth?.(newSize);
-  }, [isExpanded, setCandidWidth]);
-  useEffect(() => {
-    setIsExpanded(true);
-  }, [canisterId, candid, forceUpdate]);
-  useEffect(() => {
-    const handleMessage = ({ origin, source, data }) => {
+  // Handle incoming messages from iframe
+  const handleMessage = useCallback(
+    ({ origin, source, data }) => {
       const frame = candidFrameRef.current;
       if (frame) {
         try {
@@ -98,10 +92,75 @@ export function CandidUI({
           console.error(e);
         }
       }
-    };
-    window.addEventListener("message", handleMessage);
+    },
+    [onMessage]
+  );
+
+  useEffect(() => {
+    const newSize = isExpanded ? "30vw" : "fit-content";
+    setCandidWidth?.(newSize);
+  }, [isExpanded, setCandidWidth]);
+  useEffect(() => {
+    setIsExpanded(true);
+  }, [canisterId, candid, forceUpdate]);
+  useEffect(() => {
+    window.addEventListener("message", handleMessage, false);
     return () => window.removeEventListener("message", handleMessage);
-  }, [onMessage, url]);
+  }, [handleMessage, onMessage, url]);
+
+  const handleLoadFrame = (event) => {
+    sendConfigMessage(event.target.contentWindow, null); // use `null` for acknowledge key
+  };
+
+  const handleOpenTab = () => {
+    const newWindow = window.open(url, "_blank");
+    if (!newWindow) {
+      console.warn("Unable to open new tab for Candid UI");
+      return;
+    }
+
+    // Limit number of message attempts
+    let remainingAttempts = 20;
+    const ack = setInterval(() => {
+      if (--remainingAttempts === 0) {
+        clearInterval(ack);
+      }
+      sendConfigMessage(newWindow, ack);
+    }, 500);
+
+    // End attempts on receive message
+    const acknowledgeListener = (event) => {
+      const { origin, source, data } = event;
+      if (
+        source === newWindow &&
+        origin === CANDID_UI_CANISTER_URL &&
+        typeof data === "string" &&
+        data.startsWith(CANDID_UI_MESSAGE_PREFIX)
+      ) {
+        clearInterval(ack);
+        window.removeEventListener("message", acknowledgeListener);
+      }
+    };
+    window.addEventListener("message", acknowledgeListener, false);
+  };
+
+  const sendConfigMessage = (newWindow: Window, acknowledge: any) => {
+    console.log("Sending config message to Candid UI");
+
+    // Configure candid using iframe message
+    const message = {
+      type: "config",
+      config: {
+        candid: candid ? btoa(candid) : undefined,
+        // TODO: add more configuration options?
+      },
+      acknowledge,
+    };
+    newWindow.postMessage(
+      `${CANDID_UI_MESSAGE_PREFIX}${JSON.stringify(message)}`,
+      CANDID_UI_CANISTER_URL
+    );
+  };
 
   return (
     <CandidPanel isExpanded={isExpanded}>
@@ -119,16 +178,14 @@ export function CandidUI({
           {isExpanded ? "CANDID UI" : null}
         </Button>
         {isExpanded ? (
-          <Button
-            onClick={() => {
-              window.open(url, "_blank");
-            }}
-          >
+          <Button onClick={handleOpenTab}>
             <OpenIcon src={iconOpen} />
           </Button>
         ) : null}
       </PanelHeader>
-      {isExpanded ? <CandidFrame ref={candidFrameRef} src={url} /> : null}
+      {isExpanded ? (
+        <CandidFrame ref={candidFrameRef} src={url} onLoad={handleLoadFrame} />
+      ) : null}
     </CandidPanel>
   );
 }
