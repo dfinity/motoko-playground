586a3f0d7aab5d67f7973ffdfc6749a52b50ecf8
Yan Chen <yan.chen@dfinity.org>
2022-08-25 13:09:58 -0700

    wrapping operator

--- src/config/monacoConfig.js
+++ src/config/monacoConfig.js
@@ -27,19 +27,25 @@ export const configureMonaco = (monaco) => {
       'continue','debug','else','false','for','from_candid','func','if','in','import',
       'module','not','null','object','or','label','let','loop','private',
       'public','return','shared','try','throw','to_candid','debug_show','query','switch',
-      'true','type','var','while','with','stable','flexible','system',
+      'true','type','var','while','with',
     ],
-    accessmodifiers: ["public", "private", "shared"],
+    accessmodifiers: [
+      "public",
+      "private",
+      "shared",
+      "stable",
+      "flexible",
+      "system",
+    ],
     // prettier-ignore
     typeKeywords: [
       'Any','None','Null','Bool','Int','Int8','Int16','Int32','Int64','Nat',
-      'Nat8','Nat16','Nat32','Nat64','Word8','Word16','Word32','Word64','Float',
-      'Char','Text','Blob','Error','Principal',
+      'Nat8','Nat16','Nat32','Nat64','Float','Char','Text','Blob','Error','Principal',
     ],
     // prettier-ignore
     operators: [
-      '=','<','>',':','<:','?','+','-','*','/','%','**','&','|','^','<<','>>',
-      '#','==','!=','>=','<=',':=','+=','-=','*=','/=','%=','**=','&=','|=',
+      '=','<','>',':','<:','?','+','-','*','/','%','**','+%','-%','*%','**%','&','|','^','<<','>>','<<>','<>>',
+      '#','==','!=','>=','<=',':=','+=','-=','*=','/=','%=','**=','+%=','-%=','*%=','**%=','&=','|=',
       '^=','<<=','>>=','#=','->',
     ],
     symbols: /[=(){}[\].,:;@#_&\-<>`?!+*\\/]/,
