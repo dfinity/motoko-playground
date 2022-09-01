062a143e4a0c38efda8efd464549a24ca15f973e
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2022-08-31 18:04:21 -0600

    Add formatter to Monaco configuration

--- src/config/monacoConfig.js
+++ src/config/monacoConfig.js
@@ -1,3 +1,27 @@
 import { configure } from "motoko/contrib/monaco";
+import prettier from "prettier";
 
-export const configureMonaco = configure;
+export const configureMonaco = (monaco) => {
+  configure(monaco);
+
+  // Asynchronously load WASM
+  import("prettier-plugin-motoko/lib/environments/web")
+    .then((motokoPlugin) => {
+      monaco.languages.registerDocumentFormattingEditProvider("motoko", {
+        provideDocumentFormattingEdits(model, options, token) {
+          const source = model.getValue();
+          const formatted = prettier.format(source, {
+            plugins: [motokoPlugin],
+            filepath: "*.mo",
+          });
+          return [
+            {
+              range: model.getFullModelRange(),
+              text: formatted,
+            },
+          ];
+        },
+      });
+    })
+    .catch((err) => console.error(err));
+};
