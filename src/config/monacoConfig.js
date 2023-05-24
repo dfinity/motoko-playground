57d2c93052d840af576ace2bcc274c9988510035
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2023-05-24 10:15:47 -0600

    Markdown error code explanations (#170)
    * Add Markdown explanations via hover provider
    
    * Refactor to use 'Range' object
    
    * Remove Markdown headers from explanations
    
    * Fix tooltip overflowing outside of editor bounding box
    
    * Show error message at top of hover widget

--- src/config/monacoConfig.js
+++ src/config/monacoConfig.js
@@ -1,6 +1,8 @@
 import { configure } from "motoko/contrib/monaco";
 import prettier from "prettier";
 
+const errorCodes = require("motoko/contrib/generated/errorCodes.json");
+
 export const configureMonaco = (monaco) => {
   configure(monaco, {
     snippets: true,
@@ -26,4 +28,32 @@ export const configureMonaco = (monaco) => {
       });
     })
     .catch((err) => console.error(err));
+
+  monaco.languages.registerHoverProvider("motoko", {
+    provideHover(model, position, ...args) {
+      for (const diag of monaco.editor.getModelMarkers()) {
+        const range = new monaco.Range(
+          diag.startLineNumber,
+          diag.startColumn,
+          diag.endLineNumber,
+          diag.endColumn
+        );
+        const explanation = errorCodes[diag.code];
+        if (explanation && range.containsPosition(position)) {
+          return {
+            range,
+            contents: [
+              {
+                value: `\`\`\`text\n${diag.message}\n\`\`\``,
+              },
+              {
+                // Remove Markdown heading from explanation
+                value: explanation.replace(/^# M[0-9]+\s+/, ""),
+              },
+            ],
+          };
+        }
+      }
+    },
+  });
 };
