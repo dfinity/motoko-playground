cbd43dd6ac4f0865fa401985560688b38525611b
Yan Chen <yan.chen@dfinity.org>
2023-06-05 19:32:17 -0700

    fix

--- service/wasm-utils/build.sh
+++ service/wasm-utils/build.sh
@@ -5,7 +5,7 @@ set -e
 if [ "$SKIP_WASM" == "true" ];
 then
   mkdir -p target/wasm32-unknown-unknown/release
-  echo -n -e '\x00\x61\x73\x6d\x01\x00\x00\x00' > target/wasm32-unknown-unknown/release/wasm_opt.wasm
+  echo -n -e '\x00\x61\x73\x6d\x01\x00\x00\x00' > target/wasm32-unknown-unknown/release/wasm_utils.wasm
   echo "skip"
 else
   cargo build --target wasm32-unknown-unknown --release --package wasm-utils
