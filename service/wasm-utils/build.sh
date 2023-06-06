f51bedc2d055278b1a7065362f9cf87c478d59eb
Yan Chen <yan.chen@dfinity.org>
2023-06-05 19:20:44 -0700

    use mops to replace vessel

--- service/wasm-utils/build.sh
+++ service/wasm-utils/build.sh
@@ -9,5 +9,4 @@ then
   echo "skip"
 else
   cargo build --target wasm32-unknown-unknown --release --package wasm-utils
-  wasm-opt --strip-debug target/wasm32-unknown-unknown/release/wasm_utils.wasm -o target/wasm32-unknown-unknown/release/wasm_opt.wasm
 fi
