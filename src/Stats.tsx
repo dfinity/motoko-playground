fb38b310063634c8f00cb347e7460b8397793ebf
Yan Chen <yan.chen@dfinity.org>
2023-09-12 12:11:02 -0700

    checkpoint

--- src/Stats.tsx
+++ src/Stats.tsx
@@ -2,6 +2,42 @@ import { useState, useEffect } from "react";
 import { backend } from "./config/actor";
 import { Chart, Pie } from "./components/Chart";
 
+/*
+Taxonomy of tags
+(*) indicates server side tags
+
+origin:
+  origin:playground
+    code source
+      example:{name}
+      file:new
+      git:{repo}
+      tag:{id}
+      post:{url}
+      upload:wasm
+  origin:dfx
+    wasm:asset     (*)
+  origin:vscode
+  origin:spawned
+
+install mode:
+  mode:install     (*)
+  mode:reinstall   (*)
+  mode:upgrde      (*)
+
+playground:
+  wasm property
+    wasm:profiling (*)
+    wasm:init_args
+  project imports
+    import:package:{repo}
+    import:canister:{id}
+  moc flags
+    moc:gc:force
+    moc:gc:{method}
+  ref:{url}
+*/
+
 function extract_slice(raw, cond) {
   const res = raw
     .filter(([name, _]) => cond(name))
@@ -30,9 +66,31 @@ function one_metric(map, title, cond) {
   const slice = extract_slice(map, cond);
   return [[title, "Wasm"]].concat(slice);
 }
+function aggregate_slice(raw, prefix) {
+  const res = raw
+    .filter(([name, _]) => name.startsWith(prefix))
+    .reduce((acc, [_, n]) => acc + Number(n), 0);
+  return res;
+}
+function generateCodeSourceData(map) {
+  const res = [
+    ["Code source", "Wasm"],
+    ["file:new", aggregate_slice(map, "file:new")],
+    ["example", aggregate_slice(map, "example:")],
+    ["tag", aggregate_slice(map, "tag:")],
+    ["git", aggregate_slice(map, "git:")],
+    ["post", aggregate_slice(map, "post:")],
+    ["wasm", aggregate_slice(map, "upload:wasm")],
+  ];
+  return res;
+}
 
 export function Stats() {
+  const [canisters, setCanisters] = useState([]);
+  const [installs, setInstalls] = useState([]);
+
   const [example, setExample] = useState([]);
+  const [codeSource, setCodeSource] = useState([]);
   const [moc, setMoc] = useState([]);
   const [ref, setRef] = useState([]);
   const [origin, setOrigin] = useState([]);
@@ -53,6 +111,7 @@ export function Stats() {
           (name) => name.startsWith("example:") || name.startsWith("file:new")
         )
       );
+      setCodeSource(generateCodeSourceData(installs));
       setRef(
         two_metric(canisters, installs, "Reference link", (name) =>
           name.startsWith("ref:")
@@ -94,13 +153,14 @@ export function Stats() {
         <h2 style={{ opacity: 0.5 }}>Usage Statistics</h2>
       </div>
       <Pie title="Install mode" data={mode} />
-      <Chart title="Example" data={example} />
-      <Chart title="Reference link" data={ref} />
+      <Pie title="Playground code source" data={codeSource} />
       <Chart title="Origin" data={origin} />
+      <Chart title="Example" data={example} />
       <Chart title="Playground code imports" data={imports} />
-      <Chart title="External package/canister imports" data={packages} />
-      <Chart title="Moc" data={moc} />
-      <Chart title="Wasm" data={wasm} />
+      <Chart title="Reference link" data={ref} />
+      <Chart title="Project imports" data={packages} />
+      <Chart title="Moc flags" data={moc} />
+      <Chart title="Wasm properties" data={wasm} />
     </>
   );
 }
