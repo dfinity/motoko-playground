72947ff0da4ddbcfc364187240fdaa5f1111bc89
Yan Chen <48968912+chenyan-dfinity@users.noreply.github.com>
2024-01-15 11:31:16 -0800

    bump moc

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ import { DeployModal, DeploySetter } from "./components/DeployModal";
 import { backend, saved } from "./config/actor";
 import { setupEditorIntegration } from "./integrations/editorIntegration";
 
-const MOC_VERSION = "0.10.3";
+const MOC_VERSION = "0.10.4";
 
 const GlobalStyles = createGlobalStyle`
   :root {
