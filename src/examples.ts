791b9708b239274ad0ecb7babed318b748975a94
Yan Chen <48968912+chenyan-dfinity@users.noreply.github.com>
2024-03-05 16:16:32 -0800

    bump moc and use README.mdx

--- src/examples.ts
+++ src/examples.ts
@@ -17,57 +17,57 @@ export const exampleProjects: ExampleProject[] = [
   {
     name: "Hello, world",
     repo: { dir: "motoko/echo/src", ...example },
-    readme: `${readmeURL}/echo/README.md`,
+    readme: `${readmeURL}/echo/README.mdx`,
   },
   {
     name: "Counter",
     repo: { dir: "motoko/counter/src", ...example },
-    readme: `${readmeURL}/counter/README.md`,
+    readme: `${readmeURL}/counter/README.mdx`,
   },
   {
     name: "Calculator",
     repo: { dir: "motoko/calc/src", ...example },
-    readme: `${readmeURL}/calc/README.md`,
+    readme: `${readmeURL}/calc/README.mdx`,
   },
   {
     name: "Who am I?",
     repo: { dir: "motoko/whoami/src", ...example },
-    readme: `${readmeURL}/whoami/README.md`,
+    readme: `${readmeURL}/whoami/README.mdx`,
   },
   {
     name: "Phone Book",
     repo: { dir: "motoko/phone-book/src/phone-book", ...example },
-    readme: `${readmeURL}/phone-book/README.md`,
+    readme: `${readmeURL}/phone-book/README.mdx`,
   },
   {
     name: "Super Heroes",
     repo: { dir: "motoko/superheroes/src/superheroes", ...example },
-    readme: `${readmeURL}/superheroes/README.md`,
+    readme: `${readmeURL}/superheroes/README.mdx`,
   },
   {
     name: "Random Maze",
     repo: { dir: "motoko/random_maze/src/random_maze", ...example },
-    readme: `${readmeURL}/random_maze/README.md`,
+    readme: `${readmeURL}/random_maze/README.mdx`,
   },
   {
     name: "Game of Life",
     repo: { dir: "motoko/life", ...example },
-    readme: `${readmeURL}/life/README.md`,
+    readme: `${readmeURL}/life/README.mdx`,
   },
   {
     name: "Publisher and Subscriber",
     repo: { dir: "motoko/pub-sub/src", ...example },
-    readme: `${readmeURL}/pub-sub/README.md`,
+    readme: `${readmeURL}/pub-sub/README.mdx`,
   },
   {
     name: "Actor Classes",
     repo: { dir: "motoko/classes/src", ...example },
-    readme: `${readmeURL}/classes/README.md`,
+    readme: `${readmeURL}/classes/README.mdx`,
   },
   {
     name: "Basic DAO",
     repo: { dir: "motoko/basic_dao/src", ...example },
-    readme: `${readmeURL}/basic_dao/README.md`,
+    readme: `${readmeURL}/basic_dao/README.mdx`,
   },
 ];
 
