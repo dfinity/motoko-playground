f7b2e8033ab55d7583dab3910393ebae61ebacb6
Yan Chen <48968912+chenyan-dfinity@users.noreply.github.com>
2024-07-12 18:18:56 -0700

    fix

--- script/deploy_app.sh
+++ script/deploy_app.sh
@@ -4,7 +4,7 @@ identity dfx "~/.config/dfx/identity/default/identity.pem";
 load "testnet.env";
 
 function is_expired(info, ttl) {
-  let now = exec_silence("date", "+%s000000000");
+  let now = exec("date", "+%s000000000", record { silence = true });
   let elapsed = sub(now, info.timestamp);
   gte(elapsed, ttl);
 };
@@ -13,8 +13,19 @@ function deploy_backend(wasm) {
   // TODO: check candid and stable types compatibility
   let info = ite(exist(backend_info), opt backend_info, null);
   let new_info = call Backend.deployCanister(info, opt record { arg = encode (); wasm_module = wasm; });
-  stringify("Deployed canister id ", new_info[0].id, " with ", new_info[1]);
+  stringify("Deployed backend canister id ", new_info[0].id, " with ", new_info[1]);
   new_info[0];
+};
+
+function build_frontend(name) {
+  "Building frontend...";
+  let project_path = stringify("frontend/", name, "/");
+  let hbs_data = stringify(project_path, "hbs_data.json");
+  let _ = output(hbs_data, stringify("{ \"canister_name\": \"backend\", \"canister_name_ident\": \"backend\", \"canister_name_process_env\": \"", backend_info.id, "\" }"));
+  let candid_file = stringify("../backend/", name, "/backend.did");
+  let declaration_path = stringify(project_path, "declarations");
+  let data_path = stringify(name, "/hbs_data.json");
+  let _ = exec("node", "bindgen.js", candid_file, declaration_path, data_path, record { cwd = "frontend" });
 };
 
 function deploy_frontend(dist) {
@@ -32,10 +43,15 @@ function deploy_frontend(dist) {
   new_info[0];
 };
 
-function __main(name, backend_wasm_path, frontend_dist_path) {
+function __main(name) {
   let env = stringify(name, ".env");
   load stringify(env, "?");
-  let backend_info = deploy_backend(file(backend_wasm_path));
-  let frontend_info = deploy_frontend(frontend_dist_path);
+  let _ = exec("bash", "../../build_motoko.sh", "main.mo", record { cwd = stringify("backend/", name) });
+  let wasm_path = stringify("backend/", name, "/backend.wasm");
+  stringify("Built ", wasm_path);
+  let backend_info = deploy_backend(file(wasm_path));
+  build_frontend(name);
+  "Built frontend";
+  let frontend_info = deploy_frontend(stringify("frontend/", name, "/dist"));
   export(env, backend_info, frontend_info);
 };
