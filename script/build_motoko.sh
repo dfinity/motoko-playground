8d9faf61ddeec60743cba014f3ff987248c51566
Yan Chen <48968912+chenyan-dfinity@users.noreply.github.com>
2024-07-29 14:07:18 -0700

    revert testnet code, keep it somewhere else

--- script/build_motoko.sh
+++
@@ -1,5 +0,0 @@
-#!bash
-
-# TODO: figure out import dependencies
-
-$(dfx cache show)/moc $1 -o backend.wasm --release --idl --stable-types --public-metadata candid:service --package base $(dfx cache show)/base
