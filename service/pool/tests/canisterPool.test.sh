5e20d31988cbc3553af7e9281dec036a2bf0e5b7
Yan Chen <48968912+chenyan-dfinity@users.noreply.github.com>
2024-08-29 17:05:13 -0700

    fix

--- service/pool/tests/canisterPool.test.sh
+++ service/pool/tests/canisterPool.test.sh
@@ -32,7 +32,7 @@ let nonce = record { timestamp = 1 : int; nonce = 1 : nat };
 let CID = call S.getCanisterId(nonce, origin);
 call S.installCode(CID, record { arg = blob ""; wasm_module = empty_wasm; mode = variant { install }; canister_id = CID.id }, record { profiling = false; is_whitelisted = false; origin = origin });
 read_state("canister", CID.id, "module_hash");
-let CID3 = call S.deployCanister(null, record { arg = blob ""; wasm_module = empty_wasm; bypass_wasm_transform = opt true });
+let CID3 = call S.deployCanister(null, opt record { arg = blob ""; wasm_module = empty_wasm; bypass_wasm_transform = opt true });
 call S.transferOwnership(CID3, vec {CID3.id; S});
 
 // Immediately expire
