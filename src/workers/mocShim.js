15eb7329e1ce6107ef4440e32135da9d52f3b586
rvanasa <ryan.vandersmith@dfinity.org>
2024-09-04 15:13:51 -0600

    Only run 'eval()' in 'loadMoc()' at most once

--- src/workers/mocShim.js
+++ src/workers/mocShim.js
@@ -1,15 +1,17 @@
-let Motoko;
+let mocPromise;
 
 export const loadMoc = async () => {
-  if (!Motoko) {
-    const scriptUrl = new URL("/moc.js", self.location.origin);
-    const response = await fetch(scriptUrl);
-    const scriptContent = await response.text();
+  if (!mocPromise) {
+    mocPromise = (async () => {
+      const scriptUrl = new URL("/moc.js", self.location.origin);
+      const response = await fetch(scriptUrl);
+      const scriptContent = await response.text();
 
-    // Execute the script content
-    self.eval(scriptContent);
+      // Execute the script content
+      self.eval(scriptContent);
 
-    Motoko = self.Motoko;
+      return self.Motoko;
+    })();
   }
-  return Motoko;
+  return mocPromise;
 };
