96a940a31aceb9a2abf9ac9c5c439e417335fc66
Yan Chen <48968912+chenyan-dfinity@users.noreply.github.com>
2024-09-03 19:30:12 -0700

    fix

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,44 +1,5 @@
 /// <reference types="vite/client" />
 
-/// <reference types="node" />
-/// <reference types="react" />
-/// <reference types="react-dom" />
-
-declare module "*.avif" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.bmp" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.gif" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.jpg" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.jpeg" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.png" {
-  const src: string;
-  export default src;
-}
-
-declare module "*.webp" {
-  const src: string;
-  export default src;
-}
-
 declare module "*.svg" {
   import * as React from "react";
 
@@ -48,19 +9,4 @@ declare module "*.svg" {
 
   const src: string;
   export default src;
-}
-
-declare module "*.module.css" {
-  const classes: { readonly [key: string]: string };
-  export default classes;
-}
-
-declare module "*.module.scss" {
-  const classes: { readonly [key: string]: string };
-  export default classes;
-}
-
-declare module "*.module.sass" {
-  const classes: { readonly [key: string]: string };
-  export default classes;
 }
