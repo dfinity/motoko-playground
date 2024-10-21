e41aeb71e212c87f2de60660aba302f6d27a8fa5
Yan Chen <48968912+chenyan-dfinity@users.noreply.github.com>
2024-10-21 15:10:20 -0700

    fix

--- src/config/actor.ts
+++ src/config/actor.ts
@@ -19,8 +19,9 @@ import { idlFactory as didjs_idl } from "./didjs.did";
 const LOCAL_PORT = 4943;
 
 const hostname = window.location.hostname;
-//const local = hostname === "127.0.0.1" || hostname.endsWith("localhost");
-const local = false;
+const local = hostname === "127.0.0.1" || hostname.endsWith("localhost");
+// only enable when testing webcontainer upload
+//const local = false;
 
 export const agent = HttpAgent.createSync({
   // Prefer calling local replica directly instead of CRA proxy
