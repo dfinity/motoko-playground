cb8ddd80b0ad458d4bc00c426af50dec0c95caf8
Gabor Greif <gabor@dfinity.org>
2025-01-30 22:28:27 +0100

    chore: sophisticate the `moc` bumper (#280)
    This is now an almost "one-click" operation :-)

--- script/bump-moc.sh
+++ script/bump-moc.sh
@@ -5,11 +5,23 @@ MOC_VERSION=$1
 echo upgrading '`moc`' to $MOC_VERSION
 echo
 
+git switch main && git pull
+
 perl -pi -e "s/MOC_VERSION = \"[0-9\.]+\"/MOC_VERSION = \"$MOC_VERSION\"/g" src/App.tsx
 
+echo '`public/moc.js`' before
+ls -l public/moc.js
 curl -L https://github.com/dfinity/motoko/releases/download/${MOC_VERSION}/moc-${MOC_VERSION}.js \
   > public/moc.js
+echo '`public/moc.js`' after bump
+ls -l public/moc.js
 
 git add src/App.tsx public/moc.js
+git switch -c bump/moc-$MOC_VERSION
 git status -uno
 git diff --staged src/App.tsx
+git commit -m 'chore: bump `moc` to v'$MOC_VERSION
+git push -u origin bump/moc-$MOC_VERSION
+echo Create PR by
+echo 'open https://github.com/dfinity/motoko-playground/pull/new/bump/moc-'$MOC_VERSION
+
