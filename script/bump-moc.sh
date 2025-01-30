c610a1de58b39d068214d791d0e58e9929c54785
Gabor Greif <gabor@dfinity.org>
2025-01-30 01:46:30 +0100

    chore: script for bumping `moc` (#278)
    ## usage: `bash script/bump-moc.sh 0.13.6`
    
    This is very basic for now, we can get fancier as we use it, e.g.
    - at start: `git switch main && git pull`
    - `git switch -c bump/moc-$MOC_VERSION`
    - git commit -m 'chore: bump `moc` to v'$MOC_VERSION
    - push it: `git push -u origin bump/moc-$MOC_VERSION`
    - make the PR: `open https://github.com/dfinity/motoko-playground/pull/new/bump/moc-$MOC_VERSION`

---
+++ script/bump-moc.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+MOC_VERSION=$1
+
+echo upgrading '`moc`' to $MOC_VERSION
+echo
+
+perl -pi -e "s/MOC_VERSION = \"[0-9\.]+\"/MOC_VERSION = \"$MOC_VERSION\"/g" src/App.tsx
+
+curl -L https://github.com/dfinity/motoko/releases/download/${MOC_VERSION}/moc-${MOC_VERSION}.js \
+  > public/moc.js
+
+git add src/App.tsx public/moc.js
+git status -uno
+git diff --staged src/App.tsx
